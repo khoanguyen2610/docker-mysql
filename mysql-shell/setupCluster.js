c0f62d13bc1212b164c4717601a7e6c4e07f6b8d
khoanguyen2610 <anhkhoa.nguyen2610@gmail.com>
2019-05-17 15:10:39 +0700

    update

--- mysql-shell/setupCluster.js
+++ mysql-shell/setupCluster.js
@@ -5,12 +5,18 @@ try {
 	print('Setting up InnoDB cluster...\n');
 	shell.connect('root@mysql-server-01:3306', dbPass)
 	var cluster = dba.createCluster(clusterName);
-	print('Adding instances to the cluster.');
-	cluster.addInstance({user: "root", host: "mysql-server-02", password: dbPass})
-	print('.');
-	cluster.addInstance({user: "root", host: "mysql-server-03", password: dbPass})
-	print('.\nInstances successfully added to the cluster.');
-	print('\nInnoDB cluster deployed successfully.\n');
+	if (typeof cluster !== 'undefined') {
+		print('Adding instances to the cluster.');
+		cluster.addInstance({user: "root", host: "mysql-server-02", password: dbPass})
+		print('.');
+		cluster.addInstance({user: "root", host: "mysql-server-03", password: dbPass})
+		print('.\nInstances successfully added to the cluster.');
+		print('\nInnoDB cluster deployed successfully.\n');
+	} else {
+		// Existed Cluster
+		var cluster = dba.getCluster();
+		cluster.rescan();
+	}
 } catch(e) {
   	print('\nThe InnoDB cluster could not be created.\n\nError: ' + e.message + '\n');
 }
